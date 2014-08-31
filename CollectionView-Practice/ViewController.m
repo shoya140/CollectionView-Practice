56375a0adfbf92262574111af14e3cea8bbb82fd
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-09-01 07:35:14 +0900

    init

--- CollectionView-Practice/ViewController.m
+++ CollectionView-Practice/ViewController.m
@@ -17,13 +17,51 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 2;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return 5;
+            break;
+            
+        case 1:
+            return 2;
+            break;
+            
+        default:
+            break;
+    }
+    return 0;
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    UILabel *sectionLabel = (UILabel *)[cell viewWithTag:1];
+    sectionLabel.text = [NSString stringWithFormat:@"section: %d",indexPath.section];
+    UILabel *rowLabel = (UILabel *)[cell viewWithTag:2];
+    rowLabel.text = [NSString stringWithFormat:@"row: %d", indexPath.row];
+    return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"Clicked %d-%d",indexPath.section,indexPath.row);
+}
+
 
 @end
